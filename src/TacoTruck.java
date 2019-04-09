61572aae6f520a5531091df6e170f623de4c8747
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-09 13:22:10 -0500

    SOMETHING

---
+++ src/TacoTruck.java
@@ -0,0 +1,19 @@
+public class TacoTruck {
+    //Author:Manuel
+    //Date: 04/09/2019
+    //Project: TACOTRUCK
+    public static void main(String [] args){
+        System.out.println("___________________________");
+        System.out.println("T                          T");
+        System.out.println("A                          A");
+        System.out.println("C                          C");
+        System.out.println("O                          O");
+        System.out.println("__________________________________________________");
+        System.out.println("T                                             T");
+        System.out.println("R                                                R");
+        System.out.println("U                                                T");
+        System.out.println("C                                                C");
+        System.out.println("K                                                K");
+        System.out.println("_______________________________________________");
+    }
+}
