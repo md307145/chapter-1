37caaebc825c078bfa6faaa123ca903dfcb84ae7
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-09 12:35:56 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,21 @@
+public class TableAndChairs {
+    //Author:Manuel
+    //Date: 04/09/2019
+    //Project: Tables and Chairs
+    public class void main (String[]  args){
+        System.out.println("C                                         C") ;
+        System.out.println("H                                         H") ;
+        System.out.println("A                                         A") ;
+        System.out.println("I            TABLETABLETABLE              I") ;
+        System.out.println("R             L            L              R") ;
+        System.out.println("CHAIRCHAIR    E            e     CHAIRCHAIR") ;
+        System.out.println("C        C    G            G     C        C") ;
+        System.out.println("H        H    L            L     H        H") ;
+        System.out.println("A        A    E            E     E        E") ;
+        System.out.println("I        I    G            G     I        I") ;
+        System.out.println("R        R    !            !     R        R") ;
+    }
+
+
+
+}
