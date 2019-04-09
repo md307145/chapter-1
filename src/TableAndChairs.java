425e0a7e548da501f48197756de786baa9fd615f
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-09 12:44:21 -0500

    Update TableAndChairs.java

--- src/TableAndChairs.java
+++ src/TableAndChairs.java
@@ -3,17 +3,20 @@ public class TableAndChairs {
     //Date: 04/09/2019
     //Project: Tables and Chairs
     public class void main (String[]  args){
-        System.out.println("C                                         C") ;
-        System.out.println("H                                         H") ;
-        System.out.println("A                                         A") ;
-        System.out.println("I            TABLETABLETABLE              I") ;
-        System.out.println("R             L            L              R") ;
-        System.out.println("CHAIRCHAIR    E            e     CHAIRCHAIR") ;
-        System.out.println("C        C    G            G     C        C") ;
-        System.out.println("H        H    L            L     H        H") ;
-        System.out.println("A        A    E            E     E        E") ;
-        System.out.println("I        I    G            G     I        I") ;
-        System.out.println("R        R    !            !     R        R") ;
+        System.out.println("C                                        C") ;
+        System.out.println("H                                        H") ;
+        System.out.println("A                                        A") ;
+        System.out.println("I            TABLETABLETABLE             I") ;
+        System.out.println("R             L           L              R") ;
+        System.out.println("CHAIRCHAIR    E           e     CHAIRCHAIR") ;
+        System.out.println("C        C    G           G     C        C") ;
+        System.out.println("H        H    L           L     H        H") ;
+        System.out.println("A        A    E           E     E        E") ;
+        System.out.println("I        I    G           G     I        I") ;
+        System.out.println("R        R    !           !     R        R") ;
+
+
+        System.out.printin("update")
     }
 
 
