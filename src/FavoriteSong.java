a1642bdf705bed4d4e3aee0f75b82b13445d90e2
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-10 13:10:38 -0500

    song

---
+++ src/FavoriteSong.java
@@ -0,0 +1,13 @@
+public class FavoriteSong {
+
+        //Author:Manuel
+        //Date: 04/09/2019
+        //Project: Favorite song
+        public static void main(String[] args) {
+            System.out.println("Somebody once told me the world is gonna roll me ");
+            System.out.println("I ain't the sharpest tool in the shed ");
+            System.out.println("She was looking kind of dumb with her finger and her thumb ");
+            System.out.println("In the shape of an L on her forehead ");
+
+        }
+    }
--- src/MovieQuoteInfo.java
+++ src/MovieQuoteInfo.java
@@ -1,7 +1,7 @@
 public class MovieQuoteInfo {
     //Author:Manuel
     //Date: 04/09/2019
-    //Project: Tables and Chairs
+    //Project: Movie Quote info
     public static void main (String [] args){
      System.out.println("i'll be back(Schwarzenegger, The Terminator) 1984 ");
 
