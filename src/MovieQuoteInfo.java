7f7588f60e40722624d88fff906cfc801dd6b99e
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-10 13:02:19 -0500

    i'll be back

---
+++ src/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo {
+    //Author:Manuel
+    //Date: 04/09/2019
+    //Project: Tables and Chairs
+    public static void main (String [] args){
+     System.out.println("i'll be back(Schwarzenegger, The Terminator) 1984 ");
+
+
+
+    }
+}
